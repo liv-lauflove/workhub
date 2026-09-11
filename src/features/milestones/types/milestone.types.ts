28b75b347932edfaab31e815b30e837a6c172f24
wsantika <wahyuputra7396@gmail.com>
2026-09-11 15:47:10 +0800

    feat(milestones): add MilestoneWithDetails type definition

--- src/features/milestones/types/milestone.types.ts
+++ src/features/milestones/types/milestone.types.ts
@@ -5,3 +5,14 @@ export type MilestoneInsert =
   Database['public']['Tables']['milestones']['Insert'];
 export type MilestoneUpdate =
   Database['public']['Tables']['milestones']['Update'];
+
+export interface MilestoneWithDetails extends Milestone {
+  pic?: {
+    id: string;
+    full_name: string;
+    avatar_url: string | null;
+  } | null;
+  projects?: {
+    count: number;
+  }[];
+}
